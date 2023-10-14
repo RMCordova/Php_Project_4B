ae4e50bf595a509a56a4b07778c5ed9cb30cb729
Cordova_RM <cordovaroselymae@gmail.com>
2023-10-14 20:00:47 +0800

    4th commit

---
+++ AddMenuTest.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Add Menu</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+
+    .form-group label {
+    display: inline-block;
+    width: 200px;
+    text-align: left;
+    margin-left: 30px;
+    margin-bottom: 10px;
+    }
+
+    .form-group input {
+    display: inline-block;
+    width: 500px;
+    margin-left: 10px;
+    text-align: left;
+    margin-left: 30px;
+    margin-bottom: 10px;
+    }
+
+    .btn-container {
+    display: flex;
+    justify-content: left;
+    margin-top: 20px;
+    margin-left: 30px;
+    margin-bottom: 10px;
+    }
+
+  </style>
+</head>
+
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">CORDOVA</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Manage</a></li>
+        <li><a href="#">Menu</a></li>
+      </ul>
+      <!-- <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul> -->
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <div class="container">
+    <h1>Create Menu</h1>      
+  </div>
+</div>
+
+<div class="form-group">
+  <label for="Mname">Menu Name:</label>
+  <input type="text" class="form-control" id="Mname">
+</div>
+
+<div class="form-group">
+  <label for="Mdesc">Menu Description:</label>
+  <input type="text" class="form-control" id="Mdesc">
+</div>
+
+<div class="btn-container">
+  <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Submit</a>
+</div>
+
+</body>
+</html>
--- addMenu.php
+++ addMenu.php
@@ -1,89 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Add Menu</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
+<?php
+$host = 'localhost';
+$db   = 'my_database';
+$user = 'my_user';
+$pass = 'my_password';
+$charset = 'utf8mb4';
 
-    .form-group label {
-    display: inline-block;
-    width: 200px;
-    text-align: left;
-    margin-left: 30px;
-    margin-bottom: 10px;
-    }
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
 
-    .form-group input {
-    display: inline-block;
-    width: 500px;
-    margin-left: 10px;
-    text-align: left;
-    margin-left: 30px;
-    margin-bottom: 10px;
-    }
+$menuName = $_POST['menuName'];
+$menuDescription = $_POST['menuDescription'];
 
-    .btn-container {
-    display: flex;
-    justify-content: left;
-    margin-top: 20px;
-    margin-left: 30px;
-    margin-bottom: 10px;
-    }
+$stmt = $pdo->prepare("INSERT INTO menus (name, description) VALUES (?, ?)");
+$success = $stmt->execute([$menuName, $menuDescription]);
 
-  </style>
-</head>
-
-<body>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">CORDOVA</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="#">Manage</a></li>
-        <li><a href="#">Menu</a></li>
-      </ul>
-      <!-- <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul> -->
-    </div>
-  </div>
-</nav>
-
-<div class="jumbotron">
-  <div class="container">
-    <h1>Create Menu</h1>      
-  </div>
-</div>
-
-<div class="form-group">
-  <label for="Mname">Menu Name:</label>
-  <input type="text" class="form-control" id="Mname">
-</div>
-
-<div class="form-group">
-  <label for="Mdesc">Menu Description:</label>
-  <input type="text" class="form-control" id="Mdesc">
-</div>
-
-<div class="btn-container">
-  <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Submit</a>
-</div>
-
-</body>
-</html>
+echo json_encode(['success' => $success]);
+?>
