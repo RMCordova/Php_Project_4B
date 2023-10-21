533a7f6f5288d79fe995ab757047a499839b2e13
Cordova_RM <cordovaroselymae@gmail.com>
2023-10-21 18:58:25 +0800

    7th commit

--- AddMenuTest.php
+++ AddMenuTest.php
@@ -85,5 +85,24 @@
   <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Submit</a>
 </div>
 
+<!-- Delete Modal -->
+<div id="deleteModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Delete Menu</h4>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to delete this menu?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-danger">Delete</button>
+      </div>
+    </div>
+
 </body>
 </html>
